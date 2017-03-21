ff24971907289385c21fdc3ff2124825bc3adf72
Felix Alexis <felixalxis14@hotmail.com>
2017-03-21 20:59:12 +0000

    Random Name

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
-
-
+        var Scripteders = ["isabella", "gabrielle", "anahi", "daryl", "lauren", "pamela", "felix"];
+        $("#studentButton").click(function(){
+            var random = Math.floor(Math.random() * Scripteders.length );
+            $("#studentDisplay").html(Scripteders[random]);
+            
+        });
 });
+
+$(document).ready(function(){
+
+        var Scripteders = ["Michael", "Mat", "Nick", "Rafi", "Jenna"];
+        $("#teacherButton").click(function(){
+            var random = Math.floor(Math.random() * Scripteders.length );
+            $("#teacherDisplay").html(Scripteders[random]);
+            
+        });
+});
+
+        
